7c71665b6cb7a6b3b04bf966be5097166d07546d
emje-dania <emje7@student.eadania.dk>
2024-09-26 18:17:43 +0200

    lives + the while-loop to retry

--- opg_QuizGame/Program.cs
+++ opg_QuizGame/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,14 +12,61 @@ namespace opg_QuizGame
     {
         static void Main(string[] args)
         {
+            int lives = 6;
+            bool play = true;
+            string response;
+
 
             //The Array of 25 words
-            string[] wordArray = { "Dose", "Rack", "Budget", "Quality", "Mill", "Neck", "Absence", "Disagree", "Clothes", "Ash", "Hierarchy", "Comfortable", "Fever", "Formula", "Language", "Suggest", "Decorative", "Test", "Applaud", "Wreck", "Explicit", "Distributor", "Camera", "Bench" };
+            string[] wordArray = { "dose", "rack", "budget", "quality", "mill", "neck", "absence", "disagree", "clothes", "ash", "hierarchy", "comfortable", "fever", "formula", "language", "suggest", "decorative", "test", "applaud", "wreck", "explicit", "distributor", "camera", "bench" };
             // The word randomizer. The string word calls the wordArray and then in the array pick randomly between the 25 words
             Random randomWord = new Random();
             string word = wordArray[randomWord.Next(0, wordArray.Length)];
 
+            
 
+            //intro
+            Console.WriteLine("Guess the word!");
+            Console.WriteLine("All words are in English");
+            Console.WriteLine("Press a letter and then enter");
+
+            
+
+
+            //The game starts
+            while (play)
+            {
+                //Here '_' get put in for however long the word is
+                string wordCompletion = new string('_', word.Length);
+
+                //TEST SKAL SLETTES
+                Console.WriteLine(wordCompletion);
+                Console.WriteLine(word);
+
+                //Keeps going until you run out of lives
+                while (lives != 0)
+                {
+
+                }
+
+                //ask the player if they want to play again. if yes (Y) then play stay true, but if no (N) then play turn false
+                Console.WriteLine("Play again? (Y/N)");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    play = true;
+                }
+                else
+                {
+                    play = false;
+                }
+
+
+            }
+            Console.WriteLine("Thanks for playing");
+            Console.ReadLine();
         }
     }
 }
