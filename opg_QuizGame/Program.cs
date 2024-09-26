ba71a56ceb987b34a6918681f7b67330e3e7443e
emje-dania <emje7@student.eadania.dk>
2024-09-26 14:12:10 +0200

    word randomizer

--- opg_QuizGame/Program.cs
+++ opg_QuizGame/Program.cs
@@ -12,7 +12,11 @@ namespace opg_QuizGame
         static void Main(string[] args)
         {
 
+            //The Array of 25 words
             string[] wordArray = { "Dose", "Rack", "Budget", "Quality", "Mill", "Neck", "Absence", "Disagree", "Clothes", "Ash", "Hierarchy", "Comfortable", "Fever", "Formula", "Language", "Suggest", "Decorative", "Test", "Applaud", "Wreck", "Explicit", "Distributor", "Camera", "Bench" };
+            // The word randomizer. The string word calls the wordArray and then in the array pick randomly between the 25 words
+            Random randomWord = new Random();
+            string word = wordArray[randomWord.Next(0, wordArray.Length)];
 
 
         }
