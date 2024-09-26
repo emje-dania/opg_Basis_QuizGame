9d083773770af257ec920c11e8e011a94ca38370
emje-dania <emje7@student.eadania.dk>
2024-09-26 14:01:06 +0200

    initialt commit + wordArray

---
+++ opg_QuizGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opg_QuizGame
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] wordArray = { "Dose", "Rack", "Budget", "Quality", "Mill", "Neck", "Absence", "Disagree", "Clothes", "Ash", "Hierarchy", "Comfortable", "Fever", "Formula", "Language", "Suggest", "Decorative", "Test", "Applaud", "Wreck", "Explicit", "Distributor", "Camera", "Bench" };
+
+
+        }
+    }
+}
