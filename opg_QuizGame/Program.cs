e275db7d69d012f488962179a8eac186274853f6
emje-dania <emje7@student.eadania.dk>
2024-09-26 20:39:48 +0200

    new intro

--- opg_QuizGame/Program.cs
+++ opg_QuizGame/Program.cs
@@ -12,43 +12,88 @@ namespace opg_QuizGame
     {
         static void Main(string[] args)
         {
-            int lives = 6;
+            // Variable liste
             bool play = true;
             string response;
+            int lives = 6;
+            string intro = $"Guess the word\nAll words are in English\nPress a letter and then enter\nRemaning lives: {lives}\n\n\n";
 
+            Console.WriteLine(intro);
+            
+           
 
             //The Array of 25 words
             string[] wordArray = { "dose", "rack", "budget", "quality", "mill", "neck", "absence", "disagree", "clothes", "ash", "hierarchy", "comfortable", "fever", "formula", "language", "suggest", "decorative", "test", "applaud", "wreck", "explicit", "distributor", "camera", "bench" };
-            // The word randomizer. The string word calls the wordArray and then in the array pick randomly between the 25 words
+
+
+            // The word randomizer. The "string word" calls the wordArray and then in the array pick randomly between the 25 words
             Random randomWord = new Random();
             string word = wordArray[randomWord.Next(0, wordArray.Length)];
 
-            
+            //needs to know how big the word is so we can put in the correct amount of "_"
+            int wordLength = word.Length;
 
-            //intro
-            Console.WriteLine("Guess the word!");
-            Console.WriteLine("All words are in English");
-            Console.WriteLine("Press a letter and then enter");
+            // an empty array for used letters
+            List<char> lettersUsed = new List<char>();
 
-            
+            StringBuilder hidden = new StringBuilder();
 
-
-            //The game starts
-            while (play)
+            for (int i = 0; i<wordLength; i++)
             {
-                //Here '_' get put in for however long the word is
-                string wordCompletion = new string('_', word.Length);
+                hidden.Append("_"); //for each character = "_"
 
-                //TEST SKAL SLETTES
-                Console.WriteLine(wordCompletion);
+               
+            }
+
+            while (play) //The game starts
+            {
+
+                //The player has 6 lives
+                /*int lives = 6;*/
+
+                
+
+                Console.WriteLine(hidden);
+
                 Console.WriteLine(word);
 
-                //Keeps going until you run out of lives
-                while (lives != 0)
+
+                
+
+                while (lives != 0) //Keeps going until you run out of lives
                 {
+                    //An empty array where the letters the player type will go in
+                    var letters = new List<string>();
+
+
+                    
+                    
+                    // check if a typed letter is in the word
+                    foreach (var usedLetter in word)
+                    {
+                        //added ".ToString" the if function could not work with a char.
+                        var letter = usedLetter.ToString();
+
+                        if (letters.Contains(letter))
+                        {
+                            //if the letter was correct: type the letter
+                            // show how many lives there are left
+                            Console.Write(letter);
+                            Console.WriteLine("Lives: " + lives);
+                        }
+                        else
+                        {
+                            //if the letter was wrong: type a "_"
+                            //show how many lives there are left
+                            Console.WriteLine("_");
+                            lives--;
+                            Console.WriteLine("Lives: " + lives);
+                        }
+                        Console.ReadLine();
+                    }
+                   
 
                 }
-
                 //ask the player if they want to play again. if yes (Y) then play stay true, but if no (N) then play turn false
                 Console.WriteLine("Play again? (Y/N)");
                 response = Console.ReadLine();
@@ -62,11 +107,13 @@ namespace opg_QuizGame
                 {
                     play = false;
                 }
+               
 
 
             }
-            Console.WriteLine("Thanks for playing");
-            Console.ReadLine();
+            Console.WriteLine("Thanks for playing!");
+            Console.ReadKey();
+
         }
     }
 }
